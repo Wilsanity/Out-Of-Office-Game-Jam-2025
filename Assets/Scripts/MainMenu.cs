17d6f717cb83a2ed77db3685ff45ae2b8c14b6b7
Daniel Wilson <217dwilson@gmail.com>
2025-08-29 16:04:05 -0500

    Fixed the main menu.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,25 +6,13 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour {
     [SerializeField] private GameObject splashScreenContainer;
     [SerializeField] private GameObject levelSelectContainer;
-    private void Start() {
-        if (levelSelectContainer) {
-            Button[] levelSelectButtons = levelSelectContainer.GetComponentsInChildren<Button>();
-            for (int i = 0; i < levelSelectButtons.Length; i++) {
-                Button button = levelSelectButtons[i];
-                int cachedI = i + 1;
-                TMP_Text buttonText = button.GetComponentInChildren<TMP_Text>();
-                if (buttonText) {
-                    buttonText.text = cachedI.ToString();
-
-                }
-
-                button.onClick.AddListener(() => SceneManager.LoadScene("Level" + cachedI.ToString()));
-            }
-        }
-    }
 
     public void GoToLevelSelect() {
         splashScreenContainer.SetActive(false);
         levelSelectContainer.SetActive(true);
+    }
+
+    public void LoadScene(string name) {
+        SceneManager.LoadScene(name);
     }
 }
